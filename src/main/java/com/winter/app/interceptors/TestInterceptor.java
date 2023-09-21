8cd1e1b368a1a5dede1798cfcffdd6d07a20075b
koehdcks <chan3_3@naver.com>
2023-09-21 16:10:15 +0900

    commit

--- src/main/java/com/winter/app/interceptors/TestInterceptor.java
+++ src/main/java/com/winter/app/interceptors/TestInterceptor.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
 
 import com.winter.app.board.notice.NoticeDAO;
 
